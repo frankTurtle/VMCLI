4123ab8aeaa273acc3338ab9f485cc50c8494a7d
Barret J. Nobel <bear.nobel@gmail.com>
2015-09-11 15:10:57 -0400

    Initial Commit

---
+++ VMCLI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VMCLI
+//
+//  Created by OSX on 9/11/15.
+//  Copyright © 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VMCLI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VMCLI
+//
+//  Created by OSX on 9/11/15.
+//  Copyright © 2015 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
